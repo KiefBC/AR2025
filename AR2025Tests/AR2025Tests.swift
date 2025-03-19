4af92d903ae68f53cd609528292ebaf6e52e573b
Kiefer <kiefhay642@outlook.com>
2025-03-19 13:37:54 -0700

    Initial Commit

---
+++ AR2025Tests/AR2025Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AR2025Tests.swift
+//  AR2025Tests
+//
+//  Created by Kiefer Hay on 2025-03-19.
+//
+
+import Testing
+@testable import AR2025
+
+struct AR2025Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ AR2025UITests/AR2025UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AR2025UITestsLaunchTests.swift
+//  AR2025UITests
+//
+//  Created by Kiefer Hay on 2025-03-19.
+//
+
+import XCTest
+
+final class AR2025UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
